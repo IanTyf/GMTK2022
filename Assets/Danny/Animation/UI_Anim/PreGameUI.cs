56fd159a2cbdc2c40918bdd30c88bb8c0c0a6583
Daniel667Liu <92137425+Daniel667Liu@users.noreply.github.com>
2022-07-17 08:59:05 -0400

    pregame scene added

---
+++ Assets/Danny/Animation/UI_Anim/PreGameUI.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PreGameUI : MonoBehaviour
+{
+    private Animator Animator;
+    public AudioSource AudioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Animator = this.gameObject.GetComponent<Animator>();
+        //AudioSource = this.gameObject.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Animator.SetFloat("WarningTime", (Animator.GetFloat("WarningTime") + Time.deltaTime));
+    }
+
+    public void SuggestionIn() //on last animation
+    {
+        Animator.SetBool("SgIn", true);
+        AudioSource.Play();
+    }
+    public void SuggestionOut()//on button
+    {
+        Animator.SetBool("SgOut", true);
+        AudioSource.Stop();
+    }
+
+    public void InsIn() 
+    {
+        Animator.SetBool("Ins", true);
+    }
+}
