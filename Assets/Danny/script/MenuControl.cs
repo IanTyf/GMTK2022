236b561df84f9210cc593fd3a1e74fb3a8c18eb4
Daniel667Liu <92137425+Daniel667Liu@users.noreply.github.com>
2022-08-05 15:06:02 -0400

    menu control code finished

--- Assets/Danny/script/MenuControl.cs
+++ Assets/Danny/script/MenuControl.cs
@@ -3,31 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
+
 public class MenuControl : MonoBehaviour
 {
 
-    public Texture[] menuTextures;
+    public Texture2D[] menuTextures;
     public Material chessBoradMat;
+    public GameObject vcam1;
 
 
     public Animator ruleBookAnimator;
     // Start is called before the first frame update
     void Start()
     {
-        
+        menuNormal();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.H)) 
+        {
+            playClick();
+        }
     }
 
     public void changeTex(int targetTexIndex) 
     {
-        chessBoradMat.SetTexture("mainTexture", menuTextures[targetTexIndex]);
+        chessBoradMat.SetTexture("_Texture2D", menuTextures[targetTexIndex]);
     }
 
+    //chessborad texture control
+    #region
     public void menuNormal() 
     {
         changeTex(0);
@@ -41,6 +48,7 @@ public class MenuControl : MonoBehaviour
     public void playClick() 
     {
         changeTex(2);
+        
     }
 
     public void quitHover()
@@ -61,5 +69,27 @@ public class MenuControl : MonoBehaviour
     {
         changeTex(7);
     }
+    #endregion
 
+
+    public void RuleHover() 
+    {
+        ruleBookAnimator.SetTrigger("ruleHover");
+    }
+
+    public void RuleHoverReset() 
+    {
+        ruleBookAnimator.ResetTrigger("ruleHover");
+    }
+
+
+    private void OnDestroy()
+    {
+        menuNormal();
+    }
+
+    public void GameBegin() 
+    {
+        vcam1.SetActive(false);
+    }
 }
