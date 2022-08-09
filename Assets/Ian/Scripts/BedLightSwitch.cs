9929d5cef8f4a1cc1a3592289ac3465d944c7e51
IanTyf <iantyf@outlook.com>
2022-08-08 20:59:49 -0400

    added lights on bed level

--- Assets/Ian/Scripts/BedLightSwitch.cs
+++ Assets/Ian/Scripts/BedLightSwitch.cs
@@ -9,12 +9,16 @@ public class BedLightSwitch : MonoBehaviour
     public Material[] mats;
     public Material defaultMat;
 
+    public GameObject pointLight1;
+    public GameObject pointLight2;
+    public GameObject pointLight3;
+
     public int currentInd;
 
     // Start is called before the first frame update
     void Start()
     {
-        currentInd = -1;
+        currentInd = 1;
     }
 
     // Update is called once per frame
@@ -30,7 +34,7 @@ public class BedLightSwitch : MonoBehaviour
         for (int i=0; i<transform.childCount; i++)
         {
             float dist = Vector3.Distance(player.transform.position, transform.GetChild(i).position);
-            if (dist < minDist && dist < 5f)
+            if (dist < minDist && dist < 1f)
             {
                 minDist = dist;
                 ind = i;
@@ -39,19 +43,55 @@ public class BedLightSwitch : MonoBehaviour
 
         if (ind != -1)
         {
-            // add 2 if possible
+            // add 7 if possible
+            ind = ind + 1 >= transform.childCount ? ind : ind + 1;
+            ind = ind + 1 >= transform.childCount ? ind : ind + 1;
+            ind = ind + 1 >= transform.childCount ? ind : ind + 1;
+            ind = ind + 1 >= transform.childCount ? ind : ind + 1;
+            ind = ind + 1 >= transform.childCount ? ind : ind + 1;
             ind = ind + 1 >= transform.childCount ? ind : ind + 1;
             ind = ind + 1 >= transform.childCount ? ind : ind + 1;
 
             if (currentInd != -1)
             {
                 transform.GetChild(currentInd).gameObject.GetComponent<MeshRenderer>().material = defaultMat;
-                transform.GetChild(currentInd).gameObject.GetComponent<Light>().enabled = false;
+                if (currentInd > 0) transform.GetChild(currentInd - 1).gameObject.GetComponent<MeshRenderer>().material = defaultMat;
+                if (currentInd < transform.childCount - 1) transform.GetChild(currentInd + 1).gameObject.GetComponent<MeshRenderer>().material = defaultMat;
+                //transform.GetChild(currentInd).gameObject.GetComponent<Light>().enabled = false;
             }
 
 
-            transform.GetChild(ind).gameObject.GetComponent<Light>().enabled = true;
-            transform.GetChild(ind).gameObject.GetComponent<MeshRenderer>().material = mats[Random.Range(0, mats.Length)];
+            //transform.GetChild(ind).gameObject.GetComponent<Light>().enabled = true;
+            int randNum1 = Random.Range(0, mats.Length);
+            transform.GetChild(ind).gameObject.GetComponent<MeshRenderer>().material = mats[randNum1];
+            pointLight1.transform.SetParent(transform.GetChild(ind), false);
+            pointLight1.GetComponent<Light>().color = mats[randNum1].color;
+
+            int randNum2 = Random.Range(0, mats.Length);
+            if (ind > 0)
+            {
+                transform.GetChild(ind - 1).gameObject.GetComponent<MeshRenderer>().material = mats[randNum2];
+                pointLight2.transform.SetParent(transform.GetChild(ind - 1), false);
+                pointLight2.GetComponent<Light>().enabled = true;
+                pointLight2.GetComponent<Light>().color = mats[randNum2].color;
+            }
+            else
+            {
+                pointLight2.GetComponent<Light>().enabled = false;
+            }
+
+            int randNum3 = Random.Range(0, mats.Length);
+            if (ind < transform.childCount - 1)
+            {
+                transform.GetChild(ind + 1).gameObject.GetComponent<MeshRenderer>().material = mats[randNum3];
+                pointLight3.transform.SetParent(transform.GetChild(ind + 1), false);
+                pointLight3.GetComponent<Light>().enabled = true;
+                pointLight3.GetComponent<Light>().color = mats[randNum3].color;
+            }
+            else
+            {
+                pointLight3.GetComponent<Light>().enabled = false;
+            }
 
             currentInd = ind;
 
