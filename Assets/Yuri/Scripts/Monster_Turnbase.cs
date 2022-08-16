6ca9dc9987fa8ba01f6ec04737116bb698234d02
YuriWang99 <yuriwang99@gmail.com>
2022-08-16 15:26:41 -0400

    update monster

--- Assets/Yuri/Scripts/Monster_Turnbase.cs
+++ Assets/Yuri/Scripts/Monster_Turnbase.cs
@@ -62,6 +62,8 @@ public class Monster_Turnbase : MonoBehaviour
     private float testingTime = 2;
     [SerializeField]
     private float testingTimer = 0;
+
+    public GameObject idlePosTrigger;
     #endregion
 
 
@@ -106,6 +108,8 @@ public class Monster_Turnbase : MonoBehaviour
         //Debug.DrawRay();
 
         //Debug.Log("angle:"+Vector3.Angle(player.transform.position - transform.position, transform.forward));
+        
+        
     }
 
     private void FixedUpdate()
